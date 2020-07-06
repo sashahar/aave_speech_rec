fef89a67b1d70c99dbef643c2de4c8ffd68f944b
Sasha Harrison <sashaharrison1997@gmail.com>
2020-07-06 14:56:31 -0700

    Update dataloader.py

--- deepspeech/dataloader.py
+++ deepspeech/dataloader.py
@@ -9,59 +9,6 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import Sampler
-from process_transcriptions import *
-
-def clean_within_all_lambda(text):
-    # fix spacing in certain spellings
-    text = re.sub('T V','TV',''.join(text))
-    text = re.sub('D C','DC',''.join(text))
-
-    # remove remaining floating non-linguistic words
-    single_paren = ['<','>', '(',')', '{','}','[',']']
-    for paren in single_paren:
-        linguistic_words  = [word for word in text.split() if paren not in word]
-        text = ' '.join(linguistic_words)
-
-    # general string cleaning
-    text = re.sub(r"([a-z])\-([a-z])", r"\1 \2", text , 0, re.IGNORECASE) # replace inter-word hyphen with space
-    #DO NO REMOVE APOSTROPHES
-    text =re.sub(r'[^\s\w$]|_', ' ',text) # replace special characters with space, except $
-    text = re.sub("\s+"," ",''.join(text)) # standardize whitespace
-
-    # update numeric numbers to strings and remove $
-    text = re.sub("ft ²", "square feet", ''.join(text))
-    text = fix_numbers(text)
-    text = re.sub("\$",'dollars',''.join(text))
-    text = re.sub("\£",'pounds',''.join(text))
-
-    # standardize spellings
-    split_words = text.split()
-    for i in range(len(pre_list)):
-        split_words = [x if x.lower() != pre_list[i] else post_list[i] for x in split_words]
-    text = ' '.join(split_words)
-
-    # deal with cardinal directions
-    split_words_dir = text.split()
-    for i in range(len(pre_cardinal)):
-        split_words_dir = [x if x != pre_cardinal[i] else post_cardinal[i] for x in split_words_dir]
-    text = ' '.join(split_words_dir)
-
-    # deal with state abbreviations
-    text = fix_state_abbrevs(text)
-    text = text.lower()
-
-    # update spacing in certain spellings
-    spacing_list_pre = ['north east', 'north west', 'south east', 'south west', 'all right']
-    spacing_list_post = ['northeast', 'northwest', 'southeast', 'southwest', 'alright']
-    for i in range(len(spacing_list_pre)):
-        text = re.sub(spacing_list_pre[i], spacing_list_post[i],''.join(text))
-
-    # remove filler words and swear words
-    remove_words = swear_words + filler_words
-    resultwords  = [word for word in text.split() if word not in remove_words]
-    result = ' '.join(resultwords)
-
-    return result.upper()
 
 audio_conf = {
     'sample_rate': 44100,  # The sample rate for the data/model features
@@ -207,3 +154,105 @@ class BucketingSampler(Sampler):
 
     def shuffle(self, epoch):
         np.random.shuffle(self.bins)
+
+#globally Accessible lists
+swear_words = ['nigga', 'niggas', 'shit', 'bitch', 'damn', 'fuck', 'fuckin', 'fucking', 'motherfuckin', 'motherfucking']
+filler_words = ['um', 'uh', 'mm', 'hm', 'ooh', 'woo', 'mhm', 'mm-hm''huh', 'ha']
+
+pre_cardinal = ['N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW']
+post_cardinal = ['North', 'East', 'South', 'West', 'Northeast', 'Northwest', 'Southeast', 'Southwest']
+
+pre_list = ['cuz', 'ok', 'o', 'till', 'yup', 'imma', 'mister', 'doctor',
+            'gonna', 'tryna',
+           'carryout', 'sawmill', 'highschool', 'worldclass',
+           'saint', 'street', 'state',
+            'avenue', 'road', 'boulevard',
+           'theatre', 'neighbour', 'neighbours', 'neighbourhood', 'programme']
+post_list = ['cause', 'okay', 'oh', 'til', 'yep', 'ima', 'mr', 'dr',
+             'going to', 'trying to',
+            'carry out', 'saw mill', 'high school', 'world class',
+             'st', 'st', 'st',
+             'ave', 'rd', 'blvd',
+             'theater', 'neighbor', 'neighbors', 'neighborhood', 'program']
+
+
+def clean_coraal_lambda(text):
+    '''
+    Applies several important transformations to raw CORAAL transcripts.
+    Changes spelling on some words that are out of vocab for speech APIs.
+    i.e. "aks" -> "ask"
+    Removes CORAAL flags like unintelligible and redacted words.
+    '''
+
+    # Relabel CORAAL words. For consideration: aks -> ask?
+    split_words = text.split()
+    split_words = [x if x != 'busses' else 'buses' for x in split_words]
+    split_words = [x if x != 'aks' else 'ask' for x in split_words]
+    split_words = [x if x != 'aksing' else 'asking' for x in split_words]
+    split_words = [x if x != 'aksed' else 'asked' for x in split_words]
+    text = ' '.join(split_words)
+
+    # remove CORAAL unintelligible flags
+    text = re.sub("\/unintelligible\/",'',''.join(text))
+    text = re.sub("\/inaudible\/",'',''.join(text))
+    text = re.sub('\/RD(.*?)\/', '',''.join(text))
+    text = re.sub('\/(\?)\1*\/', '',''.join(text))
+    text = re.sub('\[', '',text) # remove square brackets from text
+    text = re.sub('\]', '',text) # remove square brackets from text
+
+    # remove nonlinguistic markers
+    text = remove_markers(text, ['<>', '()', '{}'])
+
+    return text
+
+def clean_within_all_lambda(text):
+    # fix spacing in certain spellings
+    text = re.sub('T V','TV',''.join(text))
+    text = re.sub('D C','DC',''.join(text))
+
+    # remove remaining floating non-linguistic words
+    single_paren = ['<','>', '(',')', '{','}','[',']']
+    for paren in single_paren:
+        linguistic_words  = [word for word in text.split() if paren not in word]
+        text = ' '.join(linguistic_words)
+
+    # general string cleaning
+    text = re.sub(r"([a-z])\-([a-z])", r"\1 \2", text , 0, re.IGNORECASE) # replace inter-word hyphen with space
+    #DO NO REMOVE APOSTROPHES
+    text =re.sub(r'[^\s\w$]|_', ' ',text) # replace special characters with space, except $
+    text = re.sub("\s+"," ",''.join(text)) # standardize whitespace
+
+    # update numeric numbers to strings and remove $
+    text = re.sub("ft ²", "square feet", ''.join(text))
+    text = fix_numbers(text)
+    text = re.sub("\$",'dollars',''.join(text))
+    text = re.sub("\£",'pounds',''.join(text))
+
+    # standardize spellings
+    split_words = text.split()
+    for i in range(len(pre_list)):
+        split_words = [x if x.lower() != pre_list[i] else post_list[i] for x in split_words]
+    text = ' '.join(split_words)
+
+    # deal with cardinal directions
+    split_words_dir = text.split()
+    for i in range(len(pre_cardinal)):
+        split_words_dir = [x if x != pre_cardinal[i] else post_cardinal[i] for x in split_words_dir]
+    text = ' '.join(split_words_dir)
+
+    # deal with state abbreviations
+    text = fix_state_abbrevs(text)
+    text = text.lower()
+
+    # update spacing in certain spellings
+    spacing_list_pre = ['north east', 'north west', 'south east', 'south west', 'all right']
+    spacing_list_post = ['northeast', 'northwest', 'southeast', 'southwest', 'alright']
+    for i in range(len(spacing_list_pre)):
+        text = re.sub(spacing_list_pre[i], spacing_list_post[i],''.join(text))
+
+    # remove filler words and swear words
+    remove_words = swear_words + filler_words
+    resultwords  = [word for word in text.split() if word not in remove_words]
+    result = ' '.join(resultwords)
+
+    return result.upper()
