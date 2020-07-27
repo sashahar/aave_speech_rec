398fb0b0edaf41c366477746fdcb52fb70a311a3
Claire Pajot <cpajot@stanford.edu>
2020-07-27 17:11:37 -0400

    More seeding

--- deepspeech/train.py
+++ deepspeech/train.py
@@ -101,6 +101,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
 
     device = torch.device("cuda" if args.cuda else "cpu")
     print('using device: {}'.format(device))
