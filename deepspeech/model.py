3af659ddee1c80429ceac1cd984011d6a2ec7381
Sasha Harrison <sashaharrison1997@gmail.com>
2020-07-28 18:54:37 -0700

    Update model.py

--- deepspeech/model.py
+++ deepspeech/model.py
@@ -106,10 +106,10 @@ class DeepSpeech(nn.Module):
             rnn_input_size = 40
         else:
             rnn_input_size = int(math.floor((audio_conf['n_fft']) / 2) + 1)
-        #Calculate output size of convolutional layers
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 20 - 41) / 2 + 1)
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21) / 2 + 1)
-        rnn_input_size *= 32
+            #Calculate output size of convolutional layers
+            rnn_input_size = int(math.floor(rnn_input_size + 2 * 20 - 41) / 2 + 1)
+            rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21) / 2 + 1)
+            rnn_input_size *= 32
 
         rnns = []
 
@@ -131,7 +131,9 @@ class DeepSpeech(nn.Module):
 
     def forward(self, x, lengths):
         output_lengths = self.get_seq_lens(lengths)
-        x, _ = self.conv(x, output_lengths)
+        #SKip convolution when using MFCC features
+        if not self.use_mfcc_features:
+            x, _ = self.conv(x, output_lengths)
 
         sizes = x.size()
         x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension
