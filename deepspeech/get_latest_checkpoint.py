3efe7e2d790302af8d85d8a3704833ae5a802e1f
Sasha Harrison <sashaharrison1997@gmail.com>
2020-08-12 16:44:27 -0700

    added helper script to generate latest checkpoint

---
+++ deepspeech/get_latest_checkpoint.py
@@ -0,0 +1,29 @@
+from train import *
+from model import DeepSpeech
+import argparse
+
+parser.add_argument('--checkpoint-src', default='',
+                    help='Absolute to checkpoint that you would like to convert to latest checkpoint format')
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    if args.checkpoint_src == '' or not os.path.exists(args.checkpoint_src):
+        sys.exit('You must specify a valid checkpoint src path')
+
+
+    print("Loading checkpoint from: {}".format(save_model_params_file_latest))
+    model, optim_state, epoch, avg_loss, hidden_dim = load_saved_model(save_model_params_file_latest)
+    print("Previous checkpoint saved at epoch: ", epoch - 1)
+
+    epoch = epoch - 2
+    print("new epoch is: ", epoch)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    # load previous optimizer state if using a pretrained model
+    if optim_state is not None:
+        optimizer.load_state_dict(optim_state)
+
+    destination_path = '/'.join(args.checkpoint_src.split("/")[:-1] + ['model_checkpoint_latest.pth'])
+    print(destination_path)
+    # torch.save(DeepSpeech.serialize(model, optimizer=optimizer, epoch=epoch),
+    #                                 save_model_params_file_train_best)
