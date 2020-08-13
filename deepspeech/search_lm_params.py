efb246c83e25b4f8458f806b66ae19a6149bcdc2
Claire Pajot <cpajot@stanford.edu>
2020-08-13 16:44:36 -0700

    Parallelizing parameter search

--- deepspeech/search_lm_params.py
+++ deepspeech/search_lm_params.py
@@ -14,14 +14,31 @@ parser.add_argument('--lm-alpha-from', default=0.0, type=float, help='Language m
 parser.add_argument('--lm-alpha-to', default=3.0, type=float, help='Language model weight end tuning')
 parser.add_argument('--lm-beta-from', default=0.0, type=float, help='Language model word bonus (all words) start tuning')
 parser.add_argument('--lm-beta-to', default=0.5, type=float, help='Language model word bonus (all words) end tuning')
-parser.add_argument('--lm-num-alphas', default=45, type=float, help='Number of alpha candidates for tuning')
-parser.add_argument('--lm-num-betas', default=8, type=float, help='Number of beta candidates for tuning')
+parser.add_argument('--lm-num-alphas', default=45, type=int, help='Number of alpha candidates for tuning')
+parser.add_argument('--lm-num-betas', default=8, type=int, help='Number of beta candidates for tuning')
 parser.add_argument('--output-path', help="Where to save tuning results")
 parser.add_argument('--lm-path', help='path to ARPA-format language model', default=None)
 parser.add_argument('--char-vocab-path', default="character_vocab.json", help='Contains all characters for transcription')
+parser.add_argument('--num-workers', default=16, type=int, help='Number of parallel decodes to run')
 
-def decode_dataset(params, decoder, saved_output):
+args = parser.parse_args()
+
+	if args.lm_path is None:
+		print("error: LM must be provided for tuning")
+		sys.exit(1)
+
+	with open(args.char_vocab_path) as label_file:
+		characters = str(''.join(json.load(label_file)))
+
+saved_output = torch.load(args.saved_output)
+
+def init(characters, blank_index, lm_path):
+	global decoder
+	decoder = BeamCTCDecoder(characters, lm_path=lm_path,  blank_index=blank_index)
+
+def decode_dataset(params):
 	lm_alpha, lm_beta = params
+	global decoder
 	decoder._decoder.reset_params(lm_alpha, lm_beta)
 
 	total_cer, total_wer, num_tokens, num_chars = 0, 0, 0, 0
@@ -42,17 +59,8 @@ def decode_dataset(params, decoder, saved_output):
 	return [lm_alpha, lm_beta, wer * 100, cer * 100]
 
 if __name__ == '__main__':
-	args = parser.parse_args()
 
-	if args.lm_path is None:
-		print("error: LM must be provided for tuning")
-		sys.exit(1)
-
-	with open(args.char_vocab_path) as label_file:
-		characters = str(''.join(json.load(label_file)))
-	decoder = BeamCTCDecoder(characters, lm_path=args.lm_path)
-
-	saved_output = torch.load(args.saved_output)
+    p = Pool(args.num_workers, init, [characters, characters.index('_'), args.lm_path])
 
 	cand_alphas = np.linspace(args.lm_alpha_from, args.lm_alpha_to, args.lm_num_alphas)
 	cand_betas = np.linspace(args.lm_beta_from, args.lm_beta_to, args.lm_num_betas)
@@ -63,6 +71,9 @@ if __name__ == '__main__':
 	for params in tqdm(params_grid, total=len(params_grid)):
 		decode_results = decode_dataset(params, decoder, saved_output)
 		scores.append(list(decode_results))
+
+	for params in tqdm(p.imap(decode_dataset, params_grid), total=len(params_grid)):
+		scores.append(list(params))
 
 	print("Saving tuning results to: {}".format(args.output_path))
 	with open(args.output_path, "w") as output_file:
