b4fb61538173b58b14392eb8a38bf4746b05894c
Sasha Harrison <sashaharrison1997@gmail.com>
2020-08-12 17:05:03 -0700

    bug fixes for helper script

--- deepspeech/get_latest_checkpoint.py
+++ deepspeech/get_latest_checkpoint.py
@@ -1,7 +1,9 @@
 from train import load_saved_model
-# from model import DeepSpeech
+from model import DeepSpeech
 import argparse
 import os
+import torch
+import torch.nn as nn
 
 parser = argparse.ArgumentParser(description='Model Checkpoint Conversion')
 parser.add_argument('--checkpoint-src', default='',
@@ -16,17 +18,13 @@ if __name__ == '__main__':
 
 
     print("Loading checkpoint from: {}".format(args.checkpoint_src))
-    model, optim_state, epoch, avg_loss, hidden_dim = load_saved_model(save_model_params_file_latest)
-    print("Previous checkpoint saved at epoch: ", epoch - 1)
+    model, optim_state, epoch, avg_loss, hidden_dim = load_saved_model(args.checkpoint_src)
 
     epoch = epoch - 2
-    print("new epoch is: ", epoch)
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-    # load previous optimizer state if using a pretrained model
-    if optim_state is not None:
-        optimizer.load_state_dict(optim_state)
+    optimizer = torch.optim.SGD(model.parameters(), lr = 0.001) #lr will be overwritten by state dict
+    optimizer.load_state_dict(optim_state)
 
     destination_path = '/'.join(args.checkpoint_src.split("/")[:-1] + ['model_checkpoint_latest.pth'])
-    print(destination_path)
-    # torch.save(DeepSpeech.serialize(model, optimizer=optimizer, epoch=epoch),
-    #                                 save_model_params_file_train_best)
+    print("Writing new checkpoint to:", destination_path)
+    torch.save(DeepSpeech.serialize(model, optimizer=optimizer, epoch=epoch),
+            destination_path)
